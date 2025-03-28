70c680311f49b26477f75779ec2dfd2ce6306d14
Rafael Oliveira <rafael@etus.digital>
2025-03-28 09:30:53 -0300

    feat: start using inputs

--- frontend/src/components/UserForm.vue
+++ frontend/src/components/UserForm.vue
@@ -1,6 +1,35 @@
 <template>
     <b-dialog v-model="isEditing" :width="width" @update:model-value="changeExpanded" class="op">
-        Make form here
+      <div class="form-wrapper">
+        <h1>Adicionar Usuário</h1>
+          <div class="flex justify-between items-center gap-4 w-full">
+            <BInput
+              errorMessage="O nome precisa ter ao menos 3 caracteres  "
+              :isError="false"
+              :isTextArea="false"
+              labelValue="Name"
+              :required="false"
+              size="base"
+              textAlign="start"
+              tooltipMinWidth="none"
+              type="text"
+            />
+          </div>
+
+          <div class="flex justify-between items-center gap-4 w-full">
+            <BInput
+              errorMessage="O email precisa ser válido"
+              :isError="false"
+              :isTextArea="false"
+              labelValue="Name"
+              :required="false"
+              size="base"
+              textAlign="start"
+              tooltipMinWidth="none"
+              type="email"
+            />
+          </div>
+      </div>
     </b-dialog>
 </template>
 <script setup lang="ts">
@@ -17,7 +46,8 @@ const editingItem = {
 </script>
 
 <style>
-.op {
-    padding: 80px;
+.form-wrapper {
+    padding: 50px;
+    min-height: 500px;
 }
 </style>
