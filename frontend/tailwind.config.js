4a33d0c5ae2bd69b7d5d0605a85a8ed94daa4a38
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2025-04-01 17:11:57 -0300

    fix: fix tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,5 @@
-const plugin = require("tailwindcss/plugin");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{vue,ts}"],
-  plugins: [
-    plugin(function({ addUtilities }) {
-      addUtilities({
-        '.teste': {
-          '@apply bg-black': {}
-        },
-      })
-    })
-  ],
+  plugins: [],
 };
