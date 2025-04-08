4837a1a3be55c993c86ec82d327c5b072cc7f0e8
Rafael Oliveira <rafael@etus.digital>
2025-04-08 17:59:49 -0300

    feat: user to acocunt roles

--- apps/backend/src/entities/user-accounts.entity.ts
+++ apps/backend/src/entities/user-accounts.entity.ts
@@ -1,23 +1,39 @@
-import { Entity, Column, ManyToOne, JoinColumn, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Account } from './account.entity';
 import { User } from './user.entity';
+import { Role } from '../auth/enums/roles.enum';
 
 @Entity('users_accounts')
 export class UserAccount {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({name: 'account_id', type: 'integer' })
-    accountId: number;
+  @Column({ name: 'account_id', type: 'integer' })
+  accountId: number;
 
-    @Column({name: 'user_id', type: 'integer' })
-    userId: number;
+  @Column({ name: 'user_id', type: 'integer' })
+  userId: number;
 
-    @ManyToOne(() => Account, { eager: true })
-    @JoinColumn({ name: 'account_id' })
-    account?: Account;
+  @Column({
+    name: 'role',
+    type: 'varchar',
+    default: Role.READER,
+    nullable: true,
+  })
+  role: Role;
 
-    @ManyToOne(() => User, { eager: false })
-    @JoinColumn({ name: 'user_id' })
-    user?: User;
+  @ManyToOne(() => Account, { eager: true })
+  @JoinColumn({ name: 'account_id' })
+  account?: Account;
+
+  @ManyToOne(() => User, { eager: false })
+  @JoinColumn({ name: 'user_id' })
+  user?: User;
 }
--- apps/backend/src/modules/users/users.service.ts
+++ apps/backend/src/modules/users/users.service.ts
@@ -100,8 +100,10 @@ export class UsersService {
       await this.userRepository.update(id, { ...user, id });
 
       await this.createUserAccounts(uaAdd);
+      console.log('uaAdd', uaAdd, user);
 
       await this.deleteUserAccounts(uaDel);
+      console.log('uaDel', uaDel, user);
 
       return await this.userRepository.findOne({
         where: { id },
